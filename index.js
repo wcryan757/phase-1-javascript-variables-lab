d5c3f515c81e1d25f30c4ae4ddde63f76e96d101
William Ryan <wcryan757@gmail.com>
2021-04-30 18:32:02 -0400

    commit tree 75d3a80b75f3d25fc0fe2d91e0fd12ba03d9ded6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 
-let companyName = 'Scuber'; 
-    //const companyName = 'Scuber'
+//let companyName = 'Scuber'; 
+const companyName = 'Scuber'
 
 let mostProfitableNeighborhood = 'Chelsea'
 
